8f2c0ac22fe6b6d16f7c5579974d5f2e1bba9b55
rodineiti <rodineiguitar@gmail.com>
2018-02-06 11:53:13 -0200

    Primeiro commit na app chat_firestore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,16 @@
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+interface Post {
+  title: string;
+  content: string;
+}
+
+interface PostId {
+  id: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +18,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  
+  postsCol: AngularFirestoreCollection<Post>;
+  posts: any;
+  
+  nick: string = '';
+  user: string = '';
+  title:string;
+  content:string;
+  
+  postDoc: AngularFirestoreDocument<Post>;
+  post: Observable<Post>;
+
+  constructor(
+    private afs: AngularFirestore) {
+
+  }
+
+  ngOnInit () {    
+    // caso queira fazer pesquisa por título: this.postsCol = this.afs.collection('posts', ref => ref.where('title','==','coursetro'));
+    this.postsCol = this.afs.collection('posts', ref => ref.orderBy('createdAt'));
+    this.posts = this.postsCol.snapshotChanges()
+      .map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data() as Post;
+          const id = a.payload.doc.id;
+          return { id, data }
+        })
+      })    
+  }
+
+  addPost() {
+
+    const createdAt = new Date();
+
+    this.title = this.user;
+    if (this.title !== '' && this.content !== ''){
+      this.afs.collection('posts').add({'title': this.title, 'content': this.content, 'createdAt': createdAt });
+      this.title = '';
+      this.content = '';
+    }
+  }
+
+  deletePost(postId) {
+    this.afs.doc('posts/' + postId).delete();
+  }
+
+  entrar() {
+    if(this.nick !== '') {
+      this.user = this.nick;
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+import { FormsModule } from '@angular/forms';
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyDalbaWpwF-XP9ipfqZiF7XbqylW7zlVD0',
+  authDomain: 'firestore-b5fd9.firebaseapp.com',
+  databaseURL: 'https://firestore-b5fd9.firebaseio.com',
+  projectId: 'firestore-b5fd9',
+  storageBucket: 'firestore-b5fd9.appspot.com',
+  messagingSenderId: '273504159538'
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    FormsModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
