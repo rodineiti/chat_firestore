4679ecdb2eb5df5785129e3a281242c101e1539f
rodineiti <rodineiguitar@gmail.com>
2018-02-06 12:12:00 -0200

    reset nas config do firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,12 @@ import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { FormsModule } from '@angular/forms';
 
 var firebaseConfig = {
-  apiKey: 'AIzaSyDalbaWpwF-XP9ipfqZiF7XbqylW7zlVD0',
-  authDomain: 'firestore-b5fd9.firebaseapp.com',
-  databaseURL: 'https://firestore-b5fd9.firebaseio.com',
-  projectId: 'firestore-b5fd9',
-  storageBucket: 'firestore-b5fd9.appspot.com',
-  messagingSenderId: '273504159538'
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: ''
 };
 
 @NgModule({
